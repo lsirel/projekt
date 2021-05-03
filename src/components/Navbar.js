f5f3e5a9b06413676d73fbddf0015f8c98173f50
lsirel <lisettesirel13@gmail.com>
2021-05-04 01:53:08 +0300

    cancel dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
                             Home
                         </Link>
                     </li>
-                    <li className="nav-item-dropdown">
+                    {/* <li className="nav-item-dropdown">
                         <Link className="nav-links-dropdown" onClick={closeMobileMenu}>
                             About 
                         </Link>
@@ -31,7 +31,12 @@ function Navbar() {
                         <a href="/about">Studio</a>
                         <a href="/privacypolicy">Privacy Policy</a>
                         </div>
-                    </li> 
+                    </li>  */}
+                    <li className='nav-item'>
+                        <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                            About
+                        </Link>
+                    </li>
                     <li className='nav-item'>
                         <Link to='/team' className='nav-links' onClick={closeMobileMenu}>
                             Team
