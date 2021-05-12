bc381736ea46be99c298540e9e4e9d6601129dd8
lsirel <lisettesirel13@gmail.com>
2021-05-12 22:47:35 +0300

    payment

--- src/components/packages.js
+++ src/components/packages.js
@@ -23,7 +23,7 @@ const Packages = () => {
               <li>{packages.description}</li>
             </ul>
             <div className="openBtn">
-              <a href="https://mairu.ee/EUWiKIzl" className="card-btn"> SELECT</a>
+              <a href={packages.paymentLink} rel="noreferrer" target="_blank" className="card-btn">Select</a>
             </div>
           </div>
           ))}
