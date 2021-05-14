11cab3c79e6a93d955e5a5c0f1ccc50bcef0ee99
lsirel <lisettesirel13@gmail.com>
2021-05-14 12:57:01 +0300

    team popup update

--- src/components/team.js
+++ src/components/team.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import Popup from 'reactjs-popup';
 import './team.css';
 import fetch from '../api/fetch';
 
@@ -25,37 +24,9 @@ const Instructors = () => {
                 <img src={instructor.image} alt="{instructor.firstName}" className='image'/>
                   <div class="container">
                     <h2>{instructor.firstName} {instructor.lastName}</h2>
-                    <p className="p1">
-                      <Popup
-                        trigger={<button className="button"> READ MORE... </button>}
-                        modal
-                        nested
-                      >
-                        {close => (
-                          <div className="modal">
-                            <button className="close" onClick={close}>&times;</button>
-                              <div className="header"> {instructor.firstName} {instructor.lastName}</div>
-                                <div className="content">
-                                {' '}
-                                  {instructor.description}
-                                </div>
-                                  <div className="actions">
-                                  <span>
-                                  </span>
-                                  <button
-                                    className="button1"
-                                    onClick={() => {
-                                    console.log('modal closed ');
-                                    close();
-                                    }}
-                                  >
-                              Close
-                            </button>
-                            </div>
-                          </div>
-                        )}
-                      </Popup>
-                    </p>
+                  </div>
+                  <div class="tooltip">Read more...
+                    <span class="tooltiptext">{instructor.description}</span>
                   </div>
                 </div>
               </div>
